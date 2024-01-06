0ab1898aa1f851161dad902a2f15ff51671b1efe
rianmz-genz <adrianji1945@gmail.com>
2024-01-07 01:11:09 +0700

    Price resource

---
+++ app/Filament/Resources/PriceResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PriceResource\Pages;
+use App\Filament\Resources\PriceResource\RelationManagers;
+use App\Models\Price;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+
+class PriceResource extends Resource
+{
+    protected static ?string $model = Price::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('price')
+                    ->label('Price (/Kg)')
+                    ->placeholder('Price /Kg')
+                    ->numeric()
+                    ->required()
+                    ->maxLength(20),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('price')->formatStateUsing(fn (string $state): string => __("Rp.{{number_format($state,0,',','.')}}")),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPrices::route('/'),
+            'create' => Pages\CreatePrice::route('/create'),
+            'view' => Pages\ViewPrice::route('/{record}'),
+            'edit' => Pages\EditPrice::route('/{record}/edit'),
+        ];
+    }
+}
